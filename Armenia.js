fb4a499e804b4b8f368345a1eb3ab1167bfc0af8
ZaloLover <107362176+ZaloLover@users.noreply.github.com>
2022-12-22 18:45:33 +0100

    Add files via upload

---
+++ Armenia.js
@@ -0,0 +1,87 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+window.onload = function() {
+  // Get a reference to the pictures container element
+  var picturesContainer = document.querySelector('.pictures');
+
+  // Calculate the scroll position for the middle of the pictures
+  var scrollPosition = picturesContainer.scrollHeight / 2;
+
+  // Scroll to the middle of the pictures
+  picturesContainer.scrollTop = scrollPosition;
+}
+
+var playButton = document.querySelector('#play-button');
+    var music = document.querySelector('#music');
+
+    // Add a click event listener to the button
+    playButton.addEventListener('click', function() {
+      // Play the music
+      music.play();
+    });
+
+    const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.setAttribute("data-fromTop", cursor.offsetTop - scrollY);
+  // console.log(e)
+});
+window.addEventListener("scroll", () => {
+  const fromTop = cursor.getAttribute("data-fromTop");
+  cursor.style.top = scrollY + parseInt(fromTop) + "px";
+  console.log(scrollY);
+});
+window.addEventListener("click", () => {
+  if (cursor.classList.contains("click")) {
+    cursor.classList.remove("click");
+    void cursor.offsetWidth; // trigger a DOM reflow
+    cursor.classList.add("click");
+  } else {
+    cursor.classList.add("click");
+  }
+});
